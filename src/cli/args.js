9c6a795b9f47fd58cc37b61159f09b6a318c2108
psilocinka04 <anna170904@gmail.com>
2025-03-07 09:18:42 +0300

    cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
-const parseArgs = () => {
-    // Write your code here 
+const args = () => {
+    const argsArray = process.argv.slice(2);
+    const result = [];
+
+    for (let i = 0; i < argsArray.length; i += 2) {
+        if (argsArray[i].startsWith('--') && argsArray[i + 1]) {
+            const key = argsArray[i].slice(2);
+            const value = argsArray[i + 1];
+            result.push(`${key} is ${value}`);
+        }
+    }
+
+    console.log(result.join(', '));
 };
 
-parseArgs();
+args();
