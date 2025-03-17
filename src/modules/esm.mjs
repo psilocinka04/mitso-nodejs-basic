18971b88b2721c4d5e43383673cf48ea44fb2bbb
psilocinka04 <anna170904@gmail.com>
2025-03-17 10:07:22 +0300

    modules

---
+++ src/modules/esm.mjs
@@ -0,0 +1,17 @@
+export function sum(a, b) {
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
+
+export function multiply(a, b) {
+    return a * b;
+}
+
+export function divide(a, b) {
+    if (b === 0) throw new Error('Cannot divide by zero');
+    return a / b;
+}
+
